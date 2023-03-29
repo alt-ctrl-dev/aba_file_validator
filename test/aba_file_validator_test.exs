4f28a44187c74de6cec56861148876e84e6d30f8
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-03-29 21:47:05 +0800

    test: added unit test for getting description
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- test/aba_file_validator_test.exs
+++ test/aba_file_validator_test.exs
@@ -2,7 +2,25 @@ defmodule AbaFileValidatorTest do
   use ExUnit.Case
   doctest AbaFileValidator
 
-  test "greets the world" do
-    assert AbaFileValidator.hello() == :world
+  test "transaction code description" do
+    assert AbaFileValidator.get_transaction_code_description(11) == :not_provided
+    assert AbaFileValidator.get_transaction_code_description(13) == "Externally initiated debit items"
+    assert AbaFileValidator.get_transaction_code_description("13") == "Externally initiated debit items"
+    assert AbaFileValidator.get_transaction_code_description(50) == "Externally initiated credit items with the exception of those bearing Transaction Codes"
+    assert AbaFileValidator.get_transaction_code_description("50") == "Externally initiated credit items with the exception of those bearing Transaction Codes"
+    assert AbaFileValidator.get_transaction_code_description(51) == "Australian Government Security Interest"
+    assert AbaFileValidator.get_transaction_code_description("51") == "Australian Government Security Interest"
+    assert AbaFileValidator.get_transaction_code_description(52) == "Family Allowance"
+    assert AbaFileValidator.get_transaction_code_description("52") == "Family Allowance"
+    assert AbaFileValidator.get_transaction_code_description(53) == "Pay"
+    assert AbaFileValidator.get_transaction_code_description("53") == "Pay"
+    assert AbaFileValidator.get_transaction_code_description(54) == "Pension"
+    assert AbaFileValidator.get_transaction_code_description("54") == "Pension"
+    assert AbaFileValidator.get_transaction_code_description(55) == "Allotment"
+    assert AbaFileValidator.get_transaction_code_description("55") == "Allotment"
+    assert AbaFileValidator.get_transaction_code_description(56) == "Dividend"
+    assert AbaFileValidator.get_transaction_code_description("56") == "Dividend"
+    assert AbaFileValidator.get_transaction_code_description(57) == "Debenture/Note Interest"
+    assert AbaFileValidator.get_transaction_code_description("57") == "Debenture/Note Interest"
   end
 end
