86925b50bd0ea25f1a7e0d3c7cb7077136a1f063
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-01 17:45:09 +0800

    chore: used formatter
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- test/aba_file_validator_test.exs
+++ test/aba_file_validator_test.exs
@@ -4,12 +4,25 @@ defmodule AbaFileValidatorTest do
 
   test "transaction code description" do
     assert AbaFileValidator.get_transaction_code_description(11) == :error
-    assert AbaFileValidator.get_transaction_code_description(13) == "Externally initiated debit items"
-    assert AbaFileValidator.get_transaction_code_description("13") == "Externally initiated debit items"
-    assert AbaFileValidator.get_transaction_code_description(50) == "Externally initiated credit items with the exception of those bearing Transaction Codes"
-    assert AbaFileValidator.get_transaction_code_description("50") == "Externally initiated credit items with the exception of those bearing Transaction Codes"
-    assert AbaFileValidator.get_transaction_code_description(51) == "Australian Government Security Interest"
-    assert AbaFileValidator.get_transaction_code_description("51") == "Australian Government Security Interest"
+
+    assert AbaFileValidator.get_transaction_code_description(13) ==
+             "Externally initiated debit items"
+
+    assert AbaFileValidator.get_transaction_code_description("13") ==
+             "Externally initiated debit items"
+
+    assert AbaFileValidator.get_transaction_code_description(50) ==
+             "Externally initiated credit items with the exception of those bearing Transaction Codes"
+
+    assert AbaFileValidator.get_transaction_code_description("50") ==
+             "Externally initiated credit items with the exception of those bearing Transaction Codes"
+
+    assert AbaFileValidator.get_transaction_code_description(51) ==
+             "Australian Government Security Interest"
+
+    assert AbaFileValidator.get_transaction_code_description("51") ==
+             "Australian Government Security Interest"
+
     assert AbaFileValidator.get_transaction_code_description(52) == "Family Allowance"
     assert AbaFileValidator.get_transaction_code_description("52") == "Family Allowance"
     assert AbaFileValidator.get_transaction_code_description(53) == "Pay"
