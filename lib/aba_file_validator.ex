0350f5636ec9297fe121113c30bb02371a3cc657
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-01 22:22:40 +0800

    feat: new method  to validate descriptive record
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- lib/aba_file_validator.ex
+++ lib/aba_file_validator.ex
@@ -1,5 +1,6 @@
 defmodule AbaFileValidator do
-  import __MODULE__.Utils, only: [correct_length?: 1]
+  import __MODULE__.Utils, only: [correct_length?: 2, string_empty?: 1, valid_date?: 1]
+
   @moduledoc """
   Documentation for `AbaFileValidator`.
   """
@@ -18,36 +19,62 @@ defmodule AbaFileValidator do
       iex> AbaFileValidator.validate_descriptive_record("1                 01CBA       test                      301500221212121227121222                                        ")
       {:error, :incorrect_starting_code}
 
+      iex> AbaFileValidator.validate_descriptive_record("0                   CBA       test                      301500221212121227121222                                        ")
+      {:error, :invalid_format}
+
       iex> AbaFileValidator.validate_descriptive_record("0                 01CBA       test                      301500221212121227121222                                        ")
       :ok
 
   """
 
-  def validate_descriptive_record(entry) when is_binary(entry) do
-    if not correct_length?(entry) do
-      {:error, :incorrect_length}
-    else
-      correct_descriptive_record?(entry)
-    end
-  end
-
-  def validate_descriptive_record(_) do
+  def validate_descriptive_record(entry) when not is_binary(entry) do
     {:error, :invalid_input}
   end
 
-  defp correct_descriptive_record?("0"<>entry) do
-    :ok
+  def validate_descriptive_record(entry) do
+    if not correct_length?(entry, 120) do
+      {:error, :incorrect_length}
+    else
+      get_descriptive_record?(entry)
+      |> case do
+        {:ok, _, _, _, _, _, _} -> :ok
+        error -> error
+      end
+    end
   end
 
-  defp correct_descriptive_record?(_) do
+  def get_descriptive_record?("0" <> entry) do
+    {first_blank, entry} = String.split_at(entry, 17)
+    {reel_sequence_number, entry} = String.split_at(entry, 2)
+    {bank_abbreviation, entry} = String.split_at(entry, 3)
+    {mid_blank, entry} = String.split_at(entry, 7)
+    {user_preferred_specification, entry} = String.split_at(entry, 26)
+    {user_id_number, entry} = String.split_at(entry, 6)
+    {description, entry} = String.split_at(entry, 12)
+    {date, last_blank} = String.split_at(entry, 6)
+
+    with true <- correct_length?(first_blank, 17),
+         true <- correct_length?(mid_blank, 7),
+         true <- correct_length?(last_blank, 40),
+         false <- string_empty?(reel_sequence_number),
+         false <- string_empty?(bank_abbreviation),
+         false <- string_empty?(user_preferred_specification),
+         false <- string_empty?(user_id_number),
+         false <- string_empty?(description),
+         true <- valid_date?(date),
+         false <- string_empty?(date) do
+      {:ok, reel_sequence_number, bank_abbreviation, user_preferred_specification, user_id_number,
+       description, date}
+    else
+      error ->
+        IO.inspect(error, label: :error)
+        {:error, :invalid_format}
+    end
+  end
+
+  def get_descriptive_record?(_) do
     {:error, :incorrect_starting_code}
   end
-
-
-
-
-
-
 
   @doc """
   Get a description for a given transaction code
