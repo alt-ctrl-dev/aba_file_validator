fea618b88f3ec8464d74adf512460cc7e73686a4
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-01 23:20:49 +0800

    refactor: added new method
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- lib/aba_file_validator.ex
+++ lib/aba_file_validator.ex
@@ -99,6 +99,73 @@ defmodule AbaFileValidator do
     end
   end
 
+  @doc """
+  Get the entries as part of the file total record
+
+  ## Examples
+
+      iex> AbaFileValidator.get_file_total_record(1)
+      {:error, :invalid_input}
+
+      iex> AbaFileValidator.get_file_total_record("11")
+      {:error, :incorrect_length}
+
+      iex> AbaFileValidator.get_file_total_record("01")
+      {:error, :incorrect_length}
+
+      iex> AbaFileValidator.get_file_total_record("1                 01CBA       test                      301500221212121227121222                                        ")
+      {:error, :incorrect_starting_code}
+
+      iex> AbaFileValidator.get_file_total_record("0                   CBA       test                      301500221212121227121222                                        ")
+      {:error, :invalid_format}
+
+      iex> AbaFileValidator.get_file_total_record("0                 01CBA       test                      301500221212121227121222                                        ")
+      {:ok, "01", "CBA", "test                      ", "301500", "221212121227", "121222"}
+
+  """
+  def get_file_total_record(entry) when not is_binary(entry) do
+    {:error, :invalid_input}
+  end
+
+  def get_file_total_record(entry) do
+    if not correct_length?(entry, 120) do
+      {:error, :incorrect_length}
+    else
+      {code, entry} = String.split_at(entry, 1)
+
+      if code != "7" do
+        {:error, :incorrect_starting_code}
+      else
+        {bsb_filler, entry} = String.split_at(entry, 7)
+        {reel_sequence_number, entry} = String.split_at(entry, 2)
+        {bank_abbreviation, entry} = String.split_at(entry, 3)
+        {mid_blank, entry} = String.split_at(entry, 7)
+        {user_preferred_specification, entry} = String.split_at(entry, 26)
+        {user_id_number, entry} = String.split_at(entry, 6)
+        {description, entry} = String.split_at(entry, 12)
+        {date, last_blank} = String.split_at(entry, 6)
+
+        with true <- code == "7",
+             true <- correct_length?(bsb_filler, 17),
+             true <- correct_length?(mid_blank, 7),
+             true <- correct_length?(last_blank, 40),
+             false <- string_empty?(reel_sequence_number),
+             false <- string_empty?(bank_abbreviation),
+             false <- string_empty?(user_preferred_specification),
+             false <- string_empty?(user_id_number),
+             false <- string_empty?(description),
+             true <- valid_date?(date),
+             false <- string_empty?(date) do
+          {:ok, reel_sequence_number, bank_abbreviation, user_preferred_specification,
+           user_id_number, description, date}
+        else
+          _error ->
+            {:error, :invalid_format}
+        end
+      end
+    end
+  end
+
   @spec get_transaction_code_description(any) :: :error | binary()
   @doc """
   Get a description for a given transaction code
