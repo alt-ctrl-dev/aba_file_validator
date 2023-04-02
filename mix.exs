d579b2252feabdb23de0717b799050244bcccf61
alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>
2023-04-02 16:27:09 +0800

    chore: updated mix package details
    Signed-off-by: alt-ctrl-dev <1557519+alt-ctrl-dev@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule AbaValidator.MixProject do
   use Mix.Project
 
+  @github_link "https://github.com/alt-ctrl-dev/aba_validator"
+
   def project do
     [
       app: :aba_validator,
-      version: "0.1.0",
-      elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "1.0.0",
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "ABA Validator",
+      source_url: @github_link
     ]
   end
 
@@ -21,8 +28,23 @@ defmodule AbaValidator.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "an Elixir library to validate an Australian Banking Association (ABA) file"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "aba_validator",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @github_link}
     ]
   end
 end
